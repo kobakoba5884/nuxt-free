c437a6a47015105cfdf62d00519f2f980a489851
kobakboa5884 <kobanminmin58@gmail.com>
2024-09-15 18:27:21 +0900

    feat: #step-8 is done.

--- tutorial/src/App.vue
+++ tutorial/src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 let id = 0
+const hideCompleted = ref(false)
 const inputModel = defineModel('inputModel')
 const todos = ref([
-  { id: id++, text: 'Learn HTML' },
-  { id: id++, text: 'Learn JavaScript' },
-  { id: id++, text: 'Learn Vue' }
+  { id: id++, text: 'Learn HTML' , done: true},
+  { id: id++, text: 'Learn JavaScript' , done: true},
+  { id: id++, text: 'Learn Vue' , done: false}
 ])
 
-const addNewTodo = () => {
+const addTodo = () => {
   todos.value.push({
     id: id++, text: inputModel.value
   })
@@ -19,18 +20,30 @@ const removeTodo = (todo) => {
   todos.value = todos.value.filter(t => t.id !== todo.id)
 }
 
+const filteredTodos  = computed(() => {
+  return hideCompleted.value ? todos.value.filter(t => !t.done) : todos.value
+})
+
 </script>
 
 <template>
   <div>
-    <input placeholder="new todo" v-model="inputModel">
-    <button @click="addNewTodo">Add Todo</button>
+    <input placeholder="new todo" v-model="inputModel" required>
+    <button @click="addTodo">Add Todo</button>
   </div>
   <hr>
   <ul>
-    <li v-for="todo in todos" :key="todo.id">
-      <span>{{ todo.text }}</span>
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
+  <button @click="hideCompleted = !hideCompleted">{{ hideCompleted ? 'Show all' : 'Hide completed' }}</button>
 </template>
+
+<style>
+.done{
+  text-decoration: line-through;
+}
+</style>
