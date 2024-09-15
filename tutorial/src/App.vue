71b57a09537571aa98801933dbe0d2bd236b61bd
kobakboa5884 <kobanminmin58@gmail.com>
2024-09-15 16:52:54 +0900

    feat: #step-7 is done.

--- tutorial/src/App.vue
+++ tutorial/src/App.vue
@@ -1,11 +1,36 @@
 <script setup>
 import { ref } from 'vue';
 
-const awesome = ref(true)
+let id = 0
+const inputModel = defineModel('inputModel')
+const todos = ref([
+  { id: id++, text: 'Learn HTML' },
+  { id: id++, text: 'Learn JavaScript' },
+  { id: id++, text: 'Learn Vue' }
+])
+
+const addNewTodo = () => {
+  todos.value.push({
+    id: id++, text: inputModel.value
+  })
+}
+
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter(t => t.id !== todo.id)
+}
+
 </script>
 
 <template>
-  <button @click="() => awesome = !awesome">toggle</button>
-  <h1 v-if="awesome">Vue is awesome!!!</h1>
-  <h1 v-else>Oh no</h1>
+  <div>
+    <input placeholder="new todo" v-model="inputModel">
+    <button @click="addNewTodo">Add Todo</button>
+  </div>
+  <hr>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <span>{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 </template>
