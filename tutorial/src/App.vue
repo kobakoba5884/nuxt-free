1068199ddb6fd00841b3598999a2e4aff653787e
kobakboa5884 <kobanminmin58@gmail.com>
2024-09-15 18:50:23 +0900

    feat: #step-9 is done.

--- tutorial/src/App.vue
+++ tutorial/src/App.vue
@@ -1,49 +1,13 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { onMounted, ref } from 'vue';
 
-let id = 0
-const hideCompleted = ref(false)
-const inputModel = defineModel('inputModel')
-const todos = ref([
-  { id: id++, text: 'Learn HTML' , done: true},
-  { id: id++, text: 'Learn JavaScript' , done: true},
-  { id: id++, text: 'Learn Vue' , done: false}
-])
+const pElementRef = ref(null)
 
-const addTodo = () => {
-  todos.value.push({
-    id: id++, text: inputModel.value
-  })
-}
-
-const removeTodo = (todo) => {
-  todos.value = todos.value.filter(t => t.id !== todo.id)
-}
-
-const filteredTodos  = computed(() => {
-  return hideCompleted.value ? todos.value.filter(t => !t.done) : todos.value
+onMounted(() => {
+  pElementRef.value.textContent = 'mounted'
 })
-
 </script>
 
 <template>
-  <div>
-    <input placeholder="new todo" v-model="inputModel" required>
-    <button @click="addTodo">Add Todo</button>
-  </div>
-  <hr>
-  <ul>
-    <li v-for="todo in filteredTodos" :key="todo.id">
-      <input type="checkbox" v-model="todo.done">
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
-      <button @click="removeTodo(todo)">X</button>
-    </li>
-  </ul>
-  <button @click="hideCompleted = !hideCompleted">{{ hideCompleted ? 'Show all' : 'Hide completed' }}</button>
+  <p ref="pElementRef">Hello</p>
 </template>
-
-<style>
-.done{
-  text-decoration: line-through;
-}
-</style>
