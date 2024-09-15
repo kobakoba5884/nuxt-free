8488094f958e4688a026a869833519116c8eee28
kobakboa5884 <kobanminmin58@gmail.com>
2024-09-15 14:59:05 +0900

    feat: #step-2 is done.

--- tutorial/src/App.vue
+++ tutorial/src/App.vue
@@ -1,3 +1,20 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+const counter = reactive({
+  count: 0
+})
+
+counter.count++
+
+const message = ref('Hello World!')
+
+message.value = 'Changed the message !'
+
+</script>
+
 <template>
-  <h1>Hello World!</h1>
+  <h1>{{ message }}</h1>
+  <p>Count is {{ counter.count }}</p>
+  <h1>{{ message.split(' ').reverse().join(' ') }}</h1>
 </template>
