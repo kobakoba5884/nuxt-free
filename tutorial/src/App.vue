b30e7760d4b9d329ec8d48832c7f8fbe87691a68
kobakboa5884 <kobanminmin58@gmail.com>
2024-09-15 19:17:00 +0900

    feat: #step-10 is done.

--- tutorial/src/App.vue
+++ tutorial/src/App.vue
@@ -1,13 +1,25 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { ref, watch } from 'vue';
 
-const pElementRef = ref(null)
+const todoId = ref(1)
+const todoData = ref(null)
 
-onMounted(() => {
-  pElementRef.value.textContent = 'mounted'
-})
+const fetchData = async () => {
+  todoData.value = null
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+  )
+  todoData.value = await response.json()
+}
+
+fetchData()
+
+watch(todoId, fetchData)
 </script>
 
 <template>
-  <p ref="pElementRef">Hello</p>
+  <p>Todo id: {{ todoId }}</p>
+  <button @click="todoId++" :disabled="!todoData">Fetch next todo</button>
+  <p v-if="!todoData">Loading...</p>
+  <pre v-else>{{ todoData }}</pre>
 </template>
